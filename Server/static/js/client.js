c7066473e0d93f232b84aff1ab44734d0549094c
Austin <austin.green@outlook.com>
2019-04-19 14:58:41 -0700

    Remove socketio

--- Server/static/js/client.js
+++ Server/static/js/client.js
@@ -2,11 +2,6 @@
 //The websocket server is the server for all gameplay functionality
 //Client contains all functionality for basic gameplay
 
-var socket = io('ws://165.22.143.177:5122');
-
-function onConnect() {
-  console.log('[WebSocket] Connected!')
-}
 var ws = new WebSocket('ws://165.22.143.177:5122');
   ws.onmessage = function (event) {
     console.log(event.data);
