197332a16c7cc345f61222b7dd2d273b79f4a299
Austin <austin.green@outlook.com>
2019-04-19 16:36:14 -0700

    socket handshakes

--- Server/static/js/client.js
+++ Server/static/js/client.js
@@ -1,10 +1,25 @@
-//Client for connecting to websocket server
-//The websocket server is the server for all gameplay functionality
-//Client contains all functionality for basic gameplay
+// Client for connecting to websocket server
+// The websocket server is the server for all gameplay functionality
+// Client contains all functionality for basic gameplay
+// API https://socket.io/docs/client-api/
 
 console.log('[WebSocket] Trying to connect...')
 var socket = io.connect('http://165.22.143.177:80');
 console.log('[WebSocket] Connected.')
 socket.on('connect', function() {
-    socket.emit('my event', {data: 'I\'m connected!'});
+    socket.emit('gamerequest');
+});
+
+socket.on('roomfull', function() {
+  console.log('[WebSocket] Room is full :(');
+  // TODO: Tell UI room is full
+  
+  // For now just disconnect
+  socket.disconnect();
+});
+
+socket.on('canplay', function(data) {
+  console.log('Can play!');
+  console.log(data);
+  console.log(data['player']);
 });
--- Server/webserver.py
+++ Server/webserver.py
@@ -1,12 +1,12 @@
 from flask import Flask, flash, request, render_template
 from flask_socketio import SocketIO
-import os
-import ssl
+from multiprocessing import Lock
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-
+playersPlaying = 0
+joinLock = Lock()
 
 @app.route('/', methods=['GET'])
 def index():
@@ -20,9 +20,20 @@ def connect_handler():
 def disconnect_handler():
     print('[WebSocket] Client disconnected.')
 
-@socketio.on('message')
-def handle_message(message):
-    print('[WebSocket] received message: ' + message)
+@socketio.on('gamerequest')
+def gamerequest_handler():
+
+    # Check if game is open
+    joinLock.acquire()
+    print('[WebSocket] Client is requesting to join.')
+    if playersPlaying < 4:
+        print('[WebSocket] Letting them join.')
+        playersPlaying = playersPlaying + 1
+        emit('canplay', { player: playersPlaying })
+    else:
+        print('[WebSocket] Room is full, denial >:D')
+        emit('roomfull')
+    joinLock.release()
 
 if __name__ == '__main__':
     app.debug = True
