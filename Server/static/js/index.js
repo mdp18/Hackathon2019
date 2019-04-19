7a096fa9cac93c0fc6449be0b8a452053b84e00e
Austin <austin.green@outlook.com>
2019-04-19 15:38:23 -0700

    Latest

--- Server/static/js/index.js
+++ Server/static/js/index.js
@@ -3,10 +3,21 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.dom.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.sound.min.js"></script>
   <script type="text/javascript" src="../static/js/sketch.js"></script> */
-
-  if($(window).width() > 900) {
+    
+  if($(window).width() >= 900) {
     $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js');
     $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/addons/p5.dom.js');
     $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/addons/p5.sound.min.js');
     $.getScript('../static/js/sketch.js');
+    console.log("FUCKS");
+    
+    
+  }
+  
+  if($(window).width() >= 900) {
+      $('html').append('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.min.js"></script>');
+      $('html').append('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/addons/p5.dom.min.js"></script>');
+      $('html').append('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/addons/p5.sound.min.js"></script>');
+      $('html').append('<script type="text/javascript" src="../static/js/sketch.js"></script>');
+      console.log("FUCKIN");
   }
