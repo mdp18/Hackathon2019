b419d18d5056380ae2739d8cfeef3740141a17fd
Austin <austin.green@outlook.com>
2019-04-19 15:11:42 -0700

    Add index

--- Server/static/js/index.js
+++ Server/static/js/index.js
@@ -1,15 +1,16 @@
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js"></script>
+/* <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.dom.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.sound.min.js"></script>
-  <script type="text/javascript" src="../static/js/sketch.js"></script>
+  <script type="text/javascript" src="../static/js/sketch.js"></script> */
 
   $(window).on('resize', function() {
-    if (window.matchMedia("only screen and (min-width: 1240px) and (max-width: 1280px)").matches) {
-      $.getScript('changer/js/changer-1280.js');
-    } else if (window.matchMedia("only screen and (min-width: 1390px) and (max-width: 1440px)").matches) {
-      $.getScript('changer/js/changer-1440.js');
-    } else if (window.matchMedia("only screen and (min-width: 1441px) and (max-width: 1441px)").matches) {
-      $.getScript('changer/js/changer-1441.js');
+    if (window.matchMedia("only screen and (min-width: 900px)").matches) {
+      $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js');
+      $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.dom.min.js');
+      $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.sound.min.js');
+      $.getScript('../static/js/sketch.js');
+    } else {
+
     }
   });
