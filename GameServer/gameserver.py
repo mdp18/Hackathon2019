fc1b4bb787dffbdcc5dd1c2c040f8533131e2f39
Austin <austin.green@outlook.com>
2019-04-19 13:20:38 -0700

    Start client-server websocket stuff

---
+++ GameServer/gameserver.py
@@ -0,0 +1,34 @@
+import websocket
+import thread
+import time
+
+def on_message(ws, message):
+    print(f"[WebSocket] Message: f{message}")
+
+def on_error(ws, error):
+    print(f"[WebSocket] Error: {error}")
+
+def on_close(ws):
+    print("[WebSocket] Closed")
+
+def client_thread(*args):
+    for i in range(30000):
+        time.sleep(1)
+        ws.send("Hello %d" % i)
+    time.sleep(1)
+    ws.close()
+    print "[WebSocket] Disconnecting..."
+
+def on_open(ws):     
+    thread.start_new_thread(client_thread, ())
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+
+    # Start websocket server
+    ws = websocket.WebSocketApp("ws://0.0.0.0:5122/",
+                                on_message = on_message,
+                                on_error = on_error,
+                                on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
--- WebServer/static/js/client.js
+++ WebServer/static/js/client.js
@@ -1,3 +1,34 @@
 //Client for connecting to websocket server
 //The websocket server is the server for all gameplay functionality
 //Client contains all functionality for basic gameplay
+
+'use strict';
+
+const WebSocket = require('./ws-6.2.1/websocket');
+WebSocket.Server = require('./ws-6.2.1/websocket-server');
+WebSocket.Receiver = require('./ws-6.2.1/receiver');
+WebSocket.Sender = require('./ws-6.2.1/sender');
+
+module.exports = WebSocket;
+
+const ws = new WebSocket('ws://http://165.22.143.177:5122/');
+
+// Websocket initialized
+ws.on('open', function open() {
+  console.log('connected');
+  ws.send(Date.now());
+});
+
+// Close handler
+ws.on('close', function close() {
+  console.log('disconnected');
+});
+
+// Message handler
+ws.on('message', function incoming(data) {
+  console.log(`Roundtrip time: ${Date.now() - data} ms`);
+
+  setTimeout(function timeout() {
+    ws.send(Date.now());
+  }, 500);
+});
