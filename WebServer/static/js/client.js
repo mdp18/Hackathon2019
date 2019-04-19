18e423e5fd3d9cbd6bbd8e048e6cfae2f25a5cb3
Austin <austin.green@outlook.com>
2019-04-19 14:18:32 -0700

    Fix client ip

--- WebServer/static/js/client.js
+++ WebServer/static/js/client.js
@@ -2,7 +2,7 @@
 //The websocket server is the server for all gameplay functionality
 //Client contains all functionality for basic gameplay
 
-var socket = io('http://localhost');
+var socket = io('http://http://165.22.143.177:5122');
 
 function onConnect() {
   console.log('[WebSocket] Connected!')
