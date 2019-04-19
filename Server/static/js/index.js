47ac492152424b4286e866e38047176db1342bfd
Austin <austin.green@outlook.com>
2019-04-19 16:13:02 -0700

    again

--- Server/static/js/index.js
+++ Server/static/js/index.js
@@ -11,3 +11,14 @@
   if($(window).width() >= 900) {
       console.log("FUCKIN");
   }
+
+  document.addEventListener("orientationchange", function(event){
+    switch(window.orientation) 
+    {  
+        case -90: case 90:
+            console.log("Landscape")
+            break; 
+        default:
+            console.log("portrait")
+    }
+});
