377018a1e20cc44db21dbd0db550e9679cb9133c
Austin <austin.green@outlook.com>
2019-04-19 17:37:22 -0700

    UUIDS

--- Server/webserver.py
+++ Server/webserver.py
@@ -2,6 +2,7 @@ from flask import Flask, flash, request, render_template
 from flask_login import current_user, LoginManager
 from flask_socketio import SocketIO, emit, disconnect
 from multiprocessing import Lock
+import random
 import functools
 
 app = Flask(__name__)
@@ -13,13 +14,26 @@ login_manager.login_view = '/'
 
 socketio = SocketIO(app, manage_session=False)
 
-playersOpen = []
-
-# Add players
-for i in range(4):
-    playersOpen.append(1 + i)
-
+connectedUsers = {}
 playerLock = Lock()
+
+def getPlayersPlaying():
+    global connectedUsers
+    ids = []
+    for uuid in connectedUsers:
+        if connectedUsers[uuid] != -1:
+            ids.append(connectedUsers[uuid])
+    return ids
+
+def getNextPlayer(players):
+    available = [1, 2, 3, 4]
+    for element in players:
+        available.remove(element)
+
+    if len(available) > 0:
+        return available[0]
+    else:
+        return 0
 
 @app.route('/', methods=['GET'])
 def index():
@@ -29,16 +43,32 @@ def index():
 def connect_handler():
     emit('welcome')
     print('[WebSocket] Client connected!')
-    print(current_user)
-    print('test')
+    
+    # Give anonymous user a unique id
+    playerLock.acquire()
+    global connectedUsers
+
+    uuid = random.getrandbits(64)
+    while uuid in connectedUsers:
+        uuid = random.getrandbits(64)
+
+    # Set -1 for watching
+    connectedUsers[uuid] = -1
+    playerLock.release()
+
+    print(f'[WebSocket] UUID = {uuid}')
 
 @socketio.on('disconnect')
 def disconnect_handler():
     print('[WebSocket] Client disconnected.')
+
     playerLock.acquire()
-    global playersPlaying
-    if len(playersOpen) > 0:
-        playersOpen
+    global connectedUsers
+    
+    #Remove uuid
+    if current_user.id in connectedUsers:
+        del connectedUsers[current_user.id]
+
     playerLock.release()
 
 @socketio.on('gamerequest')
@@ -46,12 +76,16 @@ def gamerequest_handler():
 
     # Check if game is open
     playerLock.acquire()
-    global playersPlaying
     print('[WebSocket] Client is requesting to join.')
-    if playersPlaying < 4:
+    global connectedUsers
+    playersPlaying = getPlayersPlaying()
+    if len(playersPlaying) < 4:
         print('[WebSocket] Letting them join.')
-        playersPlaying = playersPlaying + 1
-        emit('canplay', { 'player': playersPlaying })
+        
+        # Set them as playing
+        nextPlayer = getNextPlayer(playersPlaying)
+        connectedUsers[current_user.id] = nextPlayer
+        emit('canplay', { 'player': nextPlayer })
     else:
         print('[WebSocket] Room is full, denial >:D')
         emit('roomfull')
